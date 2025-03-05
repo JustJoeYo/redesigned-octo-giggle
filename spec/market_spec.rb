db59a6486502ab7c1783f212c0eb8d3661a4e2eb
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-03-05 08:36:28 -0800

    market test update

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -1,4 +1,6 @@
-require 'spec_helper'
+require './lib/market'
+require './lib/vendor'
+require './lib/item'
 
 RSpec.configure do |config|
   config.formatter = :documentation
@@ -14,6 +16,16 @@ RSpec.describe Market do
     @item2 = Item.new({name: 'Tomato', price: "$0.50"})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    # moved here for simplicity and shortening the reusage.
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
   end
 
   describe 'initialization' do
@@ -23,40 +35,44 @@ RSpec.describe Market do
 
     it '#attributes' do
       expect(@market.name).to eq("South Pearl Street Farmers Market")
-      expect(@market.vendors).to eq([])
+      expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
     end
   end
 
   describe 'instance methods' do
     it '#add_vendor' do
-      @market.add_vendor(@vendor1)
-      @market.add_vendor(@vendor2)
-      @market.add_vendor(@vendor3)
       expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
     end
 
     it '#vendor_names' do
-      @market.add_vendor(@vendor1)
-      @market.add_vendor(@vendor2)
-      @market.add_vendor(@vendor3)
       expect(@market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
     end
 
     it '#vendors_that_sell' do
-      @vendor1.stock(@item1, 35)
-      @vendor1.stock(@item2, 7)
-      @vendor2.stock(@item4, 50)
-      @vendor2.stock(@item3, 25)
-      @vendor3.stock(@item1, 65) # annoying but necassary
-      @market.add_vendor(@vendor1)
-      @market.add_vendor(@vendor2)
-      @market.add_vendor(@vendor3)
       expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
       expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
+    end
+
+    it '#total_inventory' do
+      expected_hash = { # will only use once so not moving to before block
+        @item1 => { quantity: 100, vendors: [@vendor1, @vendor3] },
+        @item2 => { quantity: 7, vendors: [@vendor1] },
+        @item3 => { quantity: 25, vendors: [@vendor2] },
+        @item4 => { quantity: 50, vendors: [@vendor2] }
+      }
+      expect(@market.total_inventory).to eq(expected_hash)
+    end
+
+    it '#overstocked_items' do
+      expect(@market.overstocked_items).to eq([@item1])
+    end
+
+    it '#sorted_item_list' do
+      expect(@market.sorted_item_list).to eq(["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"])
     end
   end
 
   describe 'class methods' do
-    # still no class methods to test ):
+    # so lonely in here..
   end
 end
