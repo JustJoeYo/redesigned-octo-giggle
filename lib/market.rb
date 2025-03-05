f5c3609a0d2f44b2deb85abd38032b4e7e526608
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-03-05 08:48:44 -0800

    iteration 3 methods/tests/annotations

--- lib/market.rb
+++ lib/market.rb
@@ -32,4 +32,41 @@ class Market
     # {true, true, false, true} => {true, true, true}, just keeps the vendors that we want cause they have it.
     # more comments than code...
   end
+
+  # Returns a hash with items as keys and hashes as values.
+  # The sub-hash has two key/value pairs: quantity and vendors. (copy pasted)
+  def total_inventory
+    # :quantity is initialized to 0, and :vendors is initialized to an empty array.
+    inventory = Hash.new { |hash, key| hash[key] = { quantity: 0, vendors: [] } }
+  
+    # Iterate over each vendor in the @vendors array.
+    @vendors.each do |vendor|
+      # For each vendor, iterate over their inventory hash.
+      # The inventory hash has items as keys and quantities as values.
+      vendor.inventory.each do |item, quantity|
+        # Increment the amount of item in the inventory by the quantity from the vendor's inventory.
+        inventory[item][:quantity] += quantity
+        # Add the vendor to the array of vendors that sell this item.
+        inventory[item][:vendors] << vendor
+      end
+    end
+    # Return the inventory hash. Could be return inventory, but yall dont seem to use it so ill follow you.
+    inventory
+  end
+
+  # Returns an array of items that are overstocked.
+  # An item is overstocked if it is sold by more than 1 vendor AND the total quantity is greater than 50. (copy pasted)
+  def overstocked_items
+    total_inventory.select do |item, data|
+      data[:quantity] > 50 && data[:vendors].length > 1
+    end.keys
+  end
+
+  # Returns an array of names of all the items the vendors have in stock and are also sorted alphabetically.
+  # This list does not include any duplicate items. # (copy pasted)
+  def sorted_item_list
+    @vendors.flat_map do |vendor|
+      vendor.inventory.keys.map(&:name)
+    end.uniq.sort
+  end
 end
